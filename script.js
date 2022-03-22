7621446015152ae3b0c0623691ef8b99415f47cb
Mukkawar-Shlok <87766297+Mukkawar-Shlok@users.noreply.github.com>
2022-03-22 09:03:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+//question 1
+const div = document.getElementById('answer1')
+
+const btn = document.getElementById('question1')
+
+
+btn.onclick = () => {
+    if (div.style.display !== "none") {
+        div.style.display = "none"
+
+    } else {
+        div.style.display = "block"
+
+    }
+}
+
+//question 2
+const div2 = document.getElementById('answer2')
+
+const btn2 = document.getElementById('question2')
+
+
+btn2.onclick = () => {
+    if (div2.style.display !== "none") {
+        div2.style.display = "none"
+
+    } else {
+        div2.style.display = "block"
+
+    }
+}
+//question 3
+const div3 = document.getElementById('answer3')
+
+const btn3 = document.getElementById('question3')
+
+
+btn3.onclick = () => {
+    if (div3.style.display !== "none") {
+        div3.style.display = "none"
+
+    } else {
+        div3.style.display = "block"
+
+    }
+}
+// //question 4
+const div4 = document.getElementById('answer4')
+
+const btn4 = document.getElementById('question4')
+
+
+btn4.onclick = () => {
+    if (div4.style.display !== "none") {
+        div4.style.display = "none"
+
+    } else {
+        div4.style.display = "block"
+
+    }
+}
+// //question 5
+const div5 = document.getElementById('answer5')
+
+const btn5 = document.getElementById('question5')
+
+
+btn5.onclick = () => {
+    if (div5.style.display !== "none") {
+        div5.style.display = "none"
+
+    } else {
+        div5.style.display = "block"
+
+    }
+}
+// //question 6
+const div6 = document.getElementById('answer6')
+
+const btn6 = document.getElementById('question6')
+
+
+btn6.onclick = () => {
+    if (div6.style.display !== "none") {
+        div6.style.display = "none"
+
+    } else {
+        div6.style.display = "block"
+
+    }
+}
